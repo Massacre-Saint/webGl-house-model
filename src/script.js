135893ba6f3fa29100d4e1b6e12197a0e8c7737d
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2024-06-15 19:28:12 -0500

    Refactored boilerplate into modularized functions

--- src/script.js
+++ src/script.js
@@ -1,97 +1,81 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { Timer } from 'three/addons/misc/Timer.js'
 import GUI from 'lil-gui'
 
-/**
- * Base
- */
-// Debug
-const gui = new GUI()
+let canvas;
+let camera;
+let renderer;
+let scene;
+let controls;
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
-
-// Scene
-const scene = new THREE.Scene()
-
-/**
- * Lights
- */
-// Ambient light
-const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
-scene.add(ambientLight)
-
-// Directional light
-const directionalLight = new THREE.DirectionalLight('#ffffff', 1.5)
-directionalLight.position.set(3, 2, -8)
-scene.add(directionalLight)
-
-/**
- * Sizes
- */
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+function createScene() {
+    // Scene
+    scene = new THREE.Scene();
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+    // Camera
+    const fov = 75;
+    const aspect = sizes.width / sizes.height;
+    const near = 0.1;
+    const far = 100;
+    camera = new THREE.PerspectiveCamera(fov,aspect, near, far);
+    camera.position.set(4, 2, 5);
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+    // Renderer
+    renderer = new THREE.WebGLRenderer({ canvas });
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 4
-camera.position.y = 2
-camera.position.z = 5
-scene.add(camera)
-
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-/**
- * Animate
- */
-const timer = new Timer()
-
-const tick = () =>
-{
-    // Timer
-    timer.update()
-    const elapsedTime = timer.getElapsed()
-
-    // Update controls
-    controls.update()
-
-    // Render
-    renderer.render(scene, camera)
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+    scene.add(camera);
+    renderer.render(scene, camera);
 }
 
-tick()
+function createLighting() {
+    const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
+    const directionalLight = new THREE.DirectionalLight('#ffffff', 1.5)
+    directionalLight.position.set(3, 2, -8)
+
+    scene.add(ambientLight, directionalLight);
+}
+
+function createControls() {
+    controls = new OrbitControls(camera, canvas);
+    controls.enableDamping = true;
+}
+
+function onWindowResize() {
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    renderer.render(scene, camera);
+}
+
+function setUpListeners() {
+    window.addEventListener('resize', onWindowResize);
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
+}
+
+function init() {
+    canvas = document.createElement('canvas');
+    document.body.appendChild(canvas);
+    createScene();
+    createControls();
+    animate();
+    setUpListeners();
+}
+
+init();
